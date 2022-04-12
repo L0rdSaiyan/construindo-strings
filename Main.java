d5b76e94d4f52b9b3c50e77754d87d3ec4de9110
L0rdSaiyan <89421533+L0rdSaiyan@users.noreply.github.com>
2022-04-12 12:49:02 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,23 +8,21 @@ public class Main
 
         StringBuilder oi = new StringBuilder(); //StringBuilder que vai ser usada para construir a fala
 
-        String goku[] = new String [12]; //Vetor de 12 índices que vai armazenar todos os caracteres da fala
+        String goku[] = new String [10]; //Vetor de 12 índices que vai armazenar todos os caracteres da fala
 
         goku[0] = "O";
-        goku[1] = "i";
-        goku[2] = " ";
-        goku[3] = "m";
-        goku[4] = "e";
-        goku[5] = "u";
-        goku[6] = " ";
-        goku[7] = "n";
-        goku[8] = "o";
-        goku[9] = "b";
-        goku[10] = "r";
-        goku[11] = "e";
+        goku[1] = "i ";
+        goku[2] = "m";
+        goku[3] = "e";
+        goku[4] = "u ";
+        goku[5] = "n";
+        goku[6] = "o";
+        goku[7] = "b";
+        goku[8] = "r";
+        goku[9] = "e";
 
   //Estrutura de repetição 'for' para a construção da String
-        for (int i=0; i<12 ;i++){
+        for (int i=0; i<10 ;i++){
 
             System.out.println(oi.append(goku[i])); //Utilizando o método .append que vai adicionar o caractere do vetor que estiver na posição da variável 'i' à StringBuilder para formar a fala.
 
